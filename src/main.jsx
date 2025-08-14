096d2bce51a62fe356b8b369ed612c5db1b57427
desarrollitocecom <desarrollitocecom@gmail.com>
2025-08-14 09:10:34 -0500

    Cambios para probar arreglo de render

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import AppRouter from './routers/AppRouter'
 import { Toaster } from 'sonner'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
-import store from './redux/store/Store'
+import store, { persistor } from './redux/store/Store'
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,12 +24,14 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <AppRouter />
-          <Toaster richColors position='bottom-right' />
-        </LocalizationProvider>
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <AppRouter />
+            <Toaster richColors position='bottom-right' />
+          </LocalizationProvider>
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
